23e356ac58ddc3c9759499af1b20ee68f9e8c94c
ileung6 <simonclaudia100@gmail.com>
2019-09-18 22:26:46 -0400

    edit 1

--- src/oceantrader/OceanTrader.java
+++ src/oceantrader/OceanTrader.java
@@ -5,17 +5,26 @@ import java.awt.*;
 
 public class OceanTrader {
 
+    Player player;
+
     public static void startGame() {
 
         JFrame window = new JFrame();
+        CardLayout cardLayout = new CardLayout();
+        JPanel cardPanel = new JPanel(cardLayout);
+
+        TitleScreen titleScreen = new TitleScreen();
+        ConfigurationScreen configScreen = new ConfigurationScreen();
+        ConfirmationScreen confirmScreen = null;
+
+        cardPanel.add(titleScreen.jpanel, "Title");
+        cardPanel.add(configScreen.jpanel, "Config");
+        cardPanel.add(confirmScreen.jpanel, "Confirm");
+
+        cardLayout.show(cardPanel, "Title");
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(1400, 1000); //some random arbitrary size
-
-        final CardLayout cardLayout = new CardLayout();
-        final JPanel cardPanel = new JPanel(cardLayout);
-
-//        cardPanel.add(YOUR JPANEL HERE) for testing
-
+        window.setSize(1400, 1000);
         window.add(cardPanel);
         window.setVisible(true);
     }
