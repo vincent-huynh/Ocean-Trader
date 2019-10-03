1dffdf2177e460c154a569ffd428ca4891d17e09
ileung6 <simonclaudia100@gmail.com>
2019-10-03 00:20:34 -0400

    fixed player travel

--- src/oceantrader/RegionPanel.java
+++ src/oceantrader/RegionPanel.java
@@ -1,6 +1,14 @@
 package oceantrader;
 
-import javax.swing.*;
+import javax.swing.AbstractListModel;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.Dimension;
