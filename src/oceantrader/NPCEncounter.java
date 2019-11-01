23426a382e6ed5d5f73b490906366e22867af00d
ileung6 <simonclaudia100@gmail.com>
2019-11-01 14:33:10 -0400

    damageship() added

--- src/oceantrader/NPCEncounter.java
+++ src/oceantrader/NPCEncounter.java
@@ -1,27 +1,20 @@
 /*
-Tbh this class probably isn't really necessary... but I thought I'd might as
-well just for readability. I'll probably merge this into some other class later
-once we have M6 finished... that is... if I'm not lazy (spoiler alert, I am D:).
-
 So this code should basically allow you to test your NPCs to make sure all the
 interactions are correct and there are no bugs. The forceEncounter variable
 will let you roll your particular NPC every time you travel, which should make
 it easier with the thousands of game restarts you'll do testing and making sure
 your code functions appropriately.
 
-To make testing EVEN EASIER you can make a temporary recursive call at the end
-of your code to initialize another encounter, that way you won't have to restart
-the game as much.
-
- Also I passed in the fuelCost variable for the cases where the player actually
- does get past the NPC and travels. If the user is able to travel, you'll want
- to utilize the following lines of code:
+Also I passed in the fuelCost variable for the cases where the player actually
+does get past the NPC and travels. If the user is able to travel, you'll want
+to utilize the following lines of code:
 
                         Travel.updateFuel(fuelCost);
                         Travel.travel();
 
-Lastly, the getOutcome() method does the calculation for the chance outcomes.
-This method is created for consistency among all the NPCs.
+Lastly, the getOutcome() method does the calculation for the chance outcomes,
+and the damageShip() method damages the player's ship. Use these in your code.
+These methods are created for consistency among all the NPCs.
 
 To begin testing your code, refer to line 56 in Travel.java
  */
@@ -87,6 +80,10 @@ public class NPCEncounter {
     }
 
     protected static void damageShip() {
-        System.out.println("tbd...");
+        Ship ship = OceanTrader.player.getShip();
+        ship.setHealth(ship.getHealth() - (int) (ship.getMaxHealth()
+                * ((rand.nextInt(30 - 10 + 1) + 10) / 100.0)));
+        OceanTrader.regionDisplay.shipDisplay
+                .updateShipDisplay(OceanTrader.player);
     }
 }
